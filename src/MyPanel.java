951c2a86d984c6bb3f6caa920fb35c88fcb9d24a
Florian Grabowski <flo.grabowski.98@googlemail.com>
2019-10-29 15:34:57 +0100

    fixed drawing and better graphs should skale for more than 4 res.

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -19,7 +19,7 @@ public class MyPanel extends JPanel {
         this.dauer = dauer;
         this.parent.setResizable(false);
         this.maxRes = this.getMaxRes();
-        this.parent.setSize((dauer*10)+6,(4*maxRes*20)+46+4);//4 for the red Border-Lines
+        this.parent.setSize((dauer*10)+6,(4*maxRes*20)+46+(4*resources[0].length));//4 for the red Border-Lines
         this.repaint();
     }
 
@@ -27,41 +27,11 @@ public class MyPanel extends JPanel {
     public void paintComponent(Graphics g){
         g.setColor(Color.darkGray);//Background Color
         g.fillRect(0,0,parent.getWidth(),parent.getHeight()); //Background
-        // System.out.println(String.valueOf(this.getWidth()-dauer*10) + ";" + String.valueOf(this.getHeight()-4*14*20));  //removable calc of size differents in pixel
 
 
-        for(int i = 0; i < resources[0].length; i++){
-            switch (i){
-                case 0: g.setColor(Color.BLUE);
-                    break;
-                case 1: g.setColor(Color.cyan);
-                    break;
-                case 2: g.setColor(Color.green);
-                    break;
-                case 3: g.setColor(Color.yellow);
-                    break;
-            }
-            g.fillRect(0,this.getHeight()/4*i,this.getWidth(),this.getHeight()/4);
-            g.setColor(Color.red);
-            Graphics2D g2 = (Graphics2D) g;
-            g2.setStroke(new BasicStroke(4));
-            g2.draw(new Line2D.Float(0,(this.getHeight()/4*i)+i,this.getWidth(),(this.getHeight()/4*i)+i));
-            //g.drawLine(0,(this.getHeight()/4*i),this.getWidth(),(this.getHeight()/4*i));
-        } // draw max Lines (resCap) and BG colors
-
-        int runNr = 0;
-        for (int[] res: resources) {
-            int resNr = 0;
-            for (int used : res) {
-                //System.out.println("at Run "+runNr+" with Recource "+resNr+": "+used);
-                g.setColor(Color.darkGray);
-                g.fillRect(runNr*10,(((maxRes*20)+2)*resNr),10,used*20);
-                resNr++;
-            }
-            runNr++;
-            if(runNr > this.dauer)break;
+        for (int i = 1 ;i <= resources[0].length;i++) {
+            paintGraph(g,i);
         }
-
     }
 
     private int getMaxRes(){
@@ -78,4 +48,76 @@ public class MyPanel extends JPanel {
         }
         return maxresult;
     }
+
+    private int getMaxRes(int resultResNr){
+        int runNr = 0;
+        int maxresult = 0;
+        for (int[] res: resources) {
+            int resNr = 1;
+            for (int i : res) {
+                if (i > maxresult && resNr == resultResNr){
+                    maxresult = i;
+                }
+                resNr++;
+            }
+            runNr++;
+            if(runNr > this.dauer)break;
+        }
+        return maxresult;
+    }
+
+    private void paintUpRect(int x, int y, int with, int height, Graphics g){
+        g.fillRect(x,this.getHeight()-height-y, with,height);
+    }
+
+    private void paintGraph(Graphics g, int  currentRow){
+        if(currentRow > this.resources[0].length || currentRow == 0){
+            throw new IllegalArgumentException("rowcount to large data not parent or 0");
+        }
+
+        switch (currentRow) {
+            case 1:
+                g.setColor(Color.BLUE);
+                break;
+            case 2:
+                g.setColor(Color.cyan);
+                break;
+            case 3:
+                g.setColor(Color.green);
+                break;
+            case 4:
+                g.setColor(Color.yellow);
+                break;
+        }
+
+        int runNr = 0;
+        for (int[] res: resources) {
+            int resNr = 1;
+            for (int unUsed : res) {
+                //System.out.println("at Run "+runNr+" with Recource "+resNr+": "+unUsed+" left");
+                if (resNr == currentRow){
+
+                    paintUpRect(runNr*10,(((currentRow-1)*(this.getHeight()/this.resources[0].length))),9,(getMaxRes(currentRow)-unUsed)*20,g);
+
+
+
+                }
+                resNr++;
+            }
+            runNr++;
+            if(runNr > this.dauer)break;
+        }
+
+        g.setColor(Color.black);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(new BasicStroke(4));
+        g2.draw(new Line2D.Float(0,(this.getHeight()/4*currentRow)+2,this.getWidth(),(this.getHeight()/4*currentRow)+2));
+
+        g.setColor(Color.red);
+        Graphics2D g3 = (Graphics2D) g;
+        g3.setStroke(new BasicStroke(4));
+        g3.draw(new Line2D.Float(0,this.getHeight()/4*currentRow-getMaxRes(currentRow)*20,this.getWidth(),this.getHeight()/4*currentRow-getMaxRes(currentRow)*20));
+    }
+
 }
+
